5761673917569d2d06a56da067b82d1e8f64bcd6
maikan <kankasem3002@gmail.com>
2024-12-01 20:39:27 +0700

    add picture profile and move work data to share data

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,9 @@ import Image from "next/image";
 
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+import { featuredWorks } from "./data/work";
 
 export default function Home() {
-  const featuredWorks = [
-    {
-      year: 2002,
-      name: "line-cakepop",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-  ];
-
   return (
     <div className="p-6">
       <div className=" flex justify-center items-center flex-col">
@@ -47,7 +13,7 @@ export default function Home() {
           {/* left block */}
           <div className="flex flex-col gap-10 lg:w-[506px] text-center lg:text-start">
             <h1 className="flex flex-col text-[44px] font-bold">
-              {` Hi, I'm Kanpech, a Software Developer`}
+              {`Hi, I'm Kanpech, a Software Developer`}
             </h1>
             <span>
               {`Welcome to my portfolio! . Here, you'll find my projects,
@@ -59,7 +25,19 @@ export default function Home() {
             </button>
           </div>
           {/* picture profile */}
-          <div className="size-56 rounded-full overflow-hidden bg-slate-500"></div>
+          <div className="relative">
+            <div className="size-56 rounded-full overflow-hidden bg-slate-500  ">
+              <AspectRatio ratio={1}>
+                <Image
+                  src={"/kanpech-profile.png"}
+                  layout="fill"
+                  alt="picture profile"
+                  className="z-[2]"
+                />
+              </AspectRatio>
+            </div>
+            <div className="size-56 rounded-full top-3 right-1.5 z-[1] bg-[#FF6464] absolute" />
+          </div>
         </div>
         <div className="lg:w-[842px] w-full flex flex-col gap-10">
           <h3 className=" text-[22px]">Featured works</h3>
@@ -68,7 +46,7 @@ export default function Home() {
             <>
               <Link href={"work/line-cakepop"}>
                 <div
-                  className="flex gap-6 hover:bg-gray-100 cursor-pointer lg:flex-row flex-col "
+                  className="flex gap-6  cursor-pointer lg:flex-row flex-col "
                   key={work.name}
                 >
                   <div className="lg:w-[246px]">
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -3,50 +3,16 @@ import Image from "next/image";
 
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+import { allWork } from "../data/work";
 
 export default function Work() {
-  const featuredWorks = [
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-    {
-      year: 2002,
-      name: "project",
-      description:
-        "dadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdassdadasdass",
-      coverImg:
-        "https://www.ntaskmanager.com/wp-content/uploads/2020/02/What-is-a-Project-1-scaled.jpg",
-    },
-  ];
-
   return (
     <div className="p-6">
       <div className=" flex justify-center items-center flex-col">
         <div className="lg:w-[842px] w-full flex flex-col gap-10">
           <h3 className=" text-[44px] font-black">Works</h3>
           {/* work card */}
-          {featuredWorks.map((work) => (
+          {allWork.map((work) => (
             <>
               <Link href={"work/line-cakepop"}>
                 <div
