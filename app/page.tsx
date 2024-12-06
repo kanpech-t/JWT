52fd5e19c849f662a5c174b6bb386ea843ae9ed6
maikan <kankasem3002@gmail.com>
2024-12-06 16:45:59 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -134,12 +134,16 @@ export default function Home() {
         </button>
       </form>
       <button
-        type="submit"
-        disabled={!magicLink || magicLink !== ""}
+        type="button"
+        disabled={!magicLink || magicLink === ""}
         onClick={() => {
           router.push(magicLink ?? "");
         }}
-        className={`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+        className={`inline-flex items-center px-4 py-2 border text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+          !magicLink || magicLink === ""
+            ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+            : "bg-indigo-600 text-white hover:bg-indigo-700 active:scale-95"
+        }`}
       >
         รับชมย้อนหลัง
       </button>
